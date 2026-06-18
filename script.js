c49fc805ded7b2215fa55285e82e02919103a027
Денис <kekulus95@gmail.com>
2026-06-18 21:45:30 +0300

    feat: добавил PWA-режим для тетриса
    - подключил manifest и theme-color
    - добавил PWA-иконку 512x512
    - зарегистрировал service worker для офлайн-кэша
    - обновил README с описанием PWA-запуска

--- script.js
+++ script.js
@@ -1162,3 +1162,9 @@ function handleGesture() {
     }
   }
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js").catch(() => {});
+  });
+}
